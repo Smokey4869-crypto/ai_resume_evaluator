3135bd43db8b15ca0c6362722d74a58ffd7b97f0
Lemoonh <anhminhnguyenle291@gmail.com>
2024-02-19 23:09:40 +1100

    setup s3 bucket uploader

--- server/src/upload/upload.controller.ts
+++ server/src/upload/upload.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('upload')
 export class UploadController {
-    @Post()
-    @UseInterceptors(FilesInterceptor('files'))
-    uploadFile(@UploadedFiles() files: Array<Express.Multer.File>) {
-        console.log(files);
-        // TODO: setup and implement uploading files to AWS S3
-    }
+  @Post()
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadFiles(
+    @UploadedFiles(
+    // used for handling SINGLE FILE only
+    //   new ParseFilePipe({
+    //     validators: [
+    //       new MaxFileSizeValidator({ maxSize: 1000 }),
+    //       new FileTypeValidator({ fileType: 'image/jpeg' }),
+    //     ],
+    //   }),
+    )
+    files: Array<Express.Multer.File>,
+  ) {
+    console.log(files);
+    // TODO: setup and implement uploading files to AWS S3
+  }
 }
